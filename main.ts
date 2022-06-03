d8cdce01208ad14b8d1c1ae351ca00d0fce429a2
masimon0011 <106531693+masimon0011@users.noreply.github.com>
2022-06-03 11:35:19 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 input.onSound(DetectedSound.Loud, function () {
-    if (false) {
+    if (true) {
         if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
             maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CW, 255)
             maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CW, 255)
